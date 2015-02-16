19b9ab4f79571267ed13f41f0675344336645bb2
Christopher E. Moore <christopher.e.moore@gmail.com>
2015-02-16 14:34:02 -0800

    added comment

--- luabindings/test/test.lua
+++ luabindings/test/test.lua
@@ -1,3 +1,11 @@
 #!/usr/bin/env luajit
 package.cpath = package.cpath .. ';?.dylib'
+--[[
+right now I've got to copy the following libs into this folder:
+	libCommon.dylib
+	libImage.dylib
+	libImageLua.dylib
+	libLuaCxx.dylib
+I should make a Make script to do that or something...
+--]]
 local img = require 'libImageLua'	-- says it's missing Common::File::getExtension 
