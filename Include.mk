55de908220c41099551d04c3068813822025b6ab
Christopher E. Moore <christopher.e.moore@gmail.com>
2015-09-08 16:43:33 +0300

    updating

--- Include.mk
+++ Include.mk
@@ -1,6 +1,6 @@
 IMAGE_PATH:=$(dir $(lastword $(MAKEFILE_LIST)))
 INCLUDE+=$(IMAGE_PATH)include
-DYNAMIC_LIBS+=$(IMAGE_PATH)dist/$(PLATFORM)/$(BUILD)/libImage.dylib
-DYNAMIC_LIBS+=/usr/local/lib/libcfitsio.2.3.36.dylib
-DYNAMIC_LIBS+=/usr/local/lib/libpng16.16.dylib
-DYNAMIC_LIBS+=/usr/local/lib/libtiff.5.dylib
+DYNAMIC_LIBS+=$(IMAGE_PATH)dist/$(PLATFORM)/$(BUILD)/libImage$(LIB_SUFFIX)j
+DYNAMIC_LIBS+=/usr/local/lib/libcfitsio.2.3.36$(LIB_SUFFIX)
+DYNAMIC_LIBS+=/usr/local/lib/libpng16.16$(LIB_SUFFIX)
+DYNAMIC_LIBS+=/usr/local/lib/libtiff.5$(LIB_SUFFIX)
