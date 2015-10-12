79ad477e7b1c8f863999b3543815b91bf3e08b6a
Christopher E. Moore <christopher.e.moore@gmail.com>
2015-10-12 12:44:19 +0300

    made a config file for picking which formats to use.  no more enabling matching stuff in Makefile and in Include.mk

--- Include.mk
+++ Include.mk
@@ -1,6 +1,17 @@
 IMAGE_PATH:=$(dir $(lastword $(MAKEFILE_LIST)))
+
+include $(IMAGE_PATH)Config.mk
+
 INCLUDE+=$(IMAGE_PATH)include
+
 DYNAMIC_LIBS+=$(IMAGE_PATH)dist/$(PLATFORM)/$(BUILD)/libImage$(LIB_SUFFIX)
+
+ifdef IMAGE_SUPPORT_FITS
 DYNAMIC_LIBS+=/usr/local/lib/libcfitsio.2.3.36$(LIB_SUFFIX)
+endif
+ifdef IMAGE_SUPPORT_PNG
 DYNAMIC_LIBS+=/usr/local/lib/libpng16.16$(LIB_SUFFIX)
+endif
+ifdef IMAGE_SUPPORT_TIFF
 DYNAMIC_LIBS+=/usr/local/lib/libtiff.5$(LIB_SUFFIX)
+endif
